bef23e50bab983bb392f5c7ccfe7cc6866c6877a
Diexied <alakaaamn@mail.com>
2025-06-19 21:15:35 +0530

    Updated checker.py with strength meter and polished feedback

--- password_checker/checker.py
+++ password_checker/checker.py
@@ -12,25 +12,28 @@ def password_checker(password):
     if any(char.isupper() for char in password):
         strength += 1
     else:
-        feedback.append("put those letters in upper case bruh! let them see some world!")
+        feedback.append("put those letters in uppercase bruh!")
     if any(char.islower() for char in password):
         strength += 1
     else:
-        feedback.append("some needs to be put down too!")
+        feedback.append("some needs to be put lowercase too!")
     if any(char.isdigit() for char in password):
         strength += 1
     else:
-        feedback.append("throw in some digits tooo, why are you being such a stingy?😒")
+        feedback.append("throw in some digits tooo, don't be such a stingy?😒")
     if any(char in "!@#$%^&*()><?}{|" for char in password):
         strength += 1
     else:
         feedback.append("add some cute details bruh!!")
+        
+    strength_meter = int((strength / 5) * 100)
+    
     if strength == 5:
-        return Fore.GREEN + "password is strong as hell bruhhh😏👌🔥" + Style.RESET_ALL 
+        return Fore.GREEN + "password is strong as hell bruhhh😏👌🔥\n" + f"password strenght: {strength_meter}% 💪\n" + Style.RESET_ALL 
     elif strength == 3:
-        return Fore.YELLOW + "password is mid ngl👀\nMissing:\n- " + "\n- ".join(feedback) + Style.RESET_ALL
+        return Fore.YELLOW + "password is mid ngl👀\nMissing:\n- " + f"password strenght: {strength_meter}% 💪\n" + "\n- ".join(feedback) + Style.RESET_ALL
     else:
-        return Fore.RED + "dude just what is this bruh?? go change it✏️😭🙏🏼\nMissing:\n- " + "\n- ".join(feedback) + Style.RESET_ALL
+        return Fore.RED + "dude just what is this bruh?? go change it✏️😭🙏🏼\nMissing:\n- " + f"password strenght: {strength_meter}% 💪\n" + "\n- ".join(feedback) + Style.RESET_ALL
 
 #real one!
 user_input = input("Enter your pasword, lets see😏 : ")
